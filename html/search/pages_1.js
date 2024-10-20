21b124b56a29f2e6cdb86c4d65e37046f9501952
AnatoliiM18 <mamaev-av18@ya.ru>
2024-10-20 23:24:39 +0400

    Add readme.md

---
+++ html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['osc_5fmath_20library_0',['OSC_Math library',['../index.html',1,'']]]
+];
--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,21 +1,24 @@
 var indexSectionsWithContent =
 {
-  0: "o",
+  0: "lo",
   1: "o",
-  2: "o"
+  2: "o",
+  3: "lo"
 };
 
 var indexSectionNames =
 {
   0: "all",
   1: "files",
-  2: "functions"
+  2: "functions",
+  3: "pages"
 };
 
 var indexSectionLabels =
 {
   0: "All",
   1: "Files",
-  2: "Functions"
+  2: "Functions",
+  3: "Pages"
 };
 
